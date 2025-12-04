4822f4a066496e975d6ebd14407878e861198b54
EFRAT-1-cohen <efratco054@gmail.com>
2025-12-04 22:57:20 +0200

    Added features and ignored .vs directory

--- TodoApi/ToDoDbContext.cs
+++ TodoApi/ToDoDbContext.cs
@@ -1,20 +1,26 @@
 using Microsoft.EntityFrameworkCore;
-// וודאי שה-namespace של המודל נכון
-// using YourProjectName.Models; 
 
 namespace TodoApi;
 
 public class ToDoDbContext : DbContext
 {
-    // הקונסטרקטור מקבל את ההגדרות מה-DI ב-Program.cs
     public ToDoDbContext(DbContextOptions<ToDoDbContext> options)
         : base(options)
     {
     }
 
-    // וודאי שהמודל TodoItem קיים ומוגדר נכון
     public DbSet<TodoItem> Tasks { get; set; } = null!;
 
-    // הגדרת שם הטבלה במסד הנתונים (כפי שהיה בקוד הקודם)
-   
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.Entity<TodoItem>(entity =>
+        {
+            entity.ToTable("Tasks");
+            entity.HasKey(e => e.Id);
+            entity.Property(e => e.Name).IsRequired().HasMaxLength(100);
+            entity.Property(e => e.IsComplete).HasColumnName("IsComplete");
+        });
+    }
 }
